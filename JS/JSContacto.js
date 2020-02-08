618f10c92dae646918254b861075f9accd4dbbd0
Emilianolight <emiliano.mujica@correo.unimet.edu.ve>
2020-02-07 22:54:56 -0400

    first commit

---
+++ JS/JSContacto.js
@@ -0,0 +1,14 @@
+
+
+var getData= function(){
+    var name= document.getElementById("name").value;
+    var numero= Number(document.getElementById("number").value);
+    var mail= document.getElementById("mail").value;
+    var msg= document.getElementById("msg").value;
+    console.log("El nombre es:" + name);
+    console.log("El número es:"+numero);
+    console.log("El email es:" + mail);
+    console.log("El mensaje es:"+ msg);
+alert("Su solicitud ha sido enviada")
+
+}
