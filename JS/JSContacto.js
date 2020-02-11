efa3a284d2a445a726b4d1d2263438ebf6d8f1ea
Emilianolight <emiliano.mujica@correo.unimet.edu.ve>
2020-02-11 19:54:34 -0400

    Yaaaa porfa

--- JS/JSContacto.js
+++ JS/JSContacto.js
@@ -5,10 +5,16 @@ var getData= function(){
     var numero= Number(document.getElementById("number").value);
     var mail= document.getElementById("mail").value;
     var msg= document.getElementById("msg").value;
+    if(name=="" || numero=="" || mail=="" || msg=="" ){
+
+        alert("No has llenado todos los campos");
+    }else{
+
     console.log("El nombre es:" + name);
     console.log("El número es:"+numero);
     console.log("El email es:" + mail);
     console.log("El mensaje es:"+ msg);
-alert("Su solicitud ha sido enviada")
+alert("Su solicitud ha sido enviada");
+    }
 
 }
